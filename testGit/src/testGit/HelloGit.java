59ea4d72e889a43d935e5801e7d96d2b9fcfffda
luozi <976468987@qq.com>
2016-11-08 17:07:08 +0800

    首次提交

---
+++ testGit/src/testGit/HelloGit.java
@@ -0,0 +1,21 @@
+package testGit;
+
+public class HelloGit {
+
+	private String testStr;
+	
+	public static void main(String[] args) {
+		System.out.println("hello Git");
+	}
+
+	public String getTestStr() {
+		return testStr;
+	}
+
+	public void setTestStr(String testStr) {
+		this.testStr = testStr;
+	}
+	
+	
+	
+}
